2452dfdab6462a67d8cc4e6c93e1a7962ee8497e
zhengyp <zhengyp36@gmail.com>
2024-08-08 01:27:08 +0800

    Optimize usage of compressor.py

--- zdb/compressor.py
+++ zdb/compressor.py
@@ -3,78 +3,101 @@
 from . import core
 
 class Compressor(object):
-    def __init__(self, compr_type):
-        if compr_type.supported:
-            self.compr_type = compr_type
-        else:
-            raise Exception('%s is not supported' % str(compr_type))
+    '''Usage:
+       1. compr_data = Compressor.<algorithm>.compress(usr_data)
+       2. usr_data = Compressor.<algorithm>.decompress(compr_data)
+       3. Algorithm options [lzjb, gzip_<1~9>, lz4]
+       4. Example: Compressor.lz4.compress(bytearray(1024))
+    '''
+    ALGORITHM_TABLE = [
+        [  0, 'ZIO_COMPRESS_INHERIT', 'inherit', False ],
+        [  1, 'ZIO_COMPRESS_ON',      'on',      False ],
+        [  2, 'ZIO_COMPRESS_OFF',     'off',     False ],
+        [  3, 'ZIO_COMPRESS_LZJB',    'lzjb',    True  ],
+        [  4, 'ZIO_COMPRESS_EMPTY',   'empty',   False ],
+        [  5, 'ZIO_COMPRESS_GZIP_1',  'gzip_1',  True  ],
+        [  6, 'ZIO_COMPRESS_GZIP_2',  'gzip_2',  True  ],
+        [  7, 'ZIO_COMPRESS_GZIP_3',  'gzip_3',  True  ],
+        [  8, 'ZIO_COMPRESS_GZIP_4',  'gzip_4',  True  ],
+        [  9, 'ZIO_COMPRESS_GZIP_5',  'gzip_5',  True  ],
+        [ 10, 'ZIO_COMPRESS_GZIP_6',  'gzip_6',  True  ],
+        [ 11, 'ZIO_COMPRESS_GZIP_7',  'gzip_7',  True  ],
+        [ 12, 'ZIO_COMPRESS_GZIP_8',  'gzip_8',  True  ],
+        [ 13, 'ZIO_COMPRESS_GZIP_9',  'gzip_9',  True  ],
+        [ 14, 'ZIO_COMPRESS_ZLE',     'zle',     True  ],
+        [ 15, 'ZIO_COMPRESS_LZ4',     'lz4',     True  ],
+        [ 16, 'ZIO_COMPRESS_ZSTD',    'zstd',    False ],
+    ]
     
-    def compress(self, usr_data):
-        mv_usr = memoryview(usr_data)
-        
-        out_buffer = memoryview(bytearray(len(mv_usr)-1))
-        out_len = core.compress(self.compr_type.value, mv_usr, out_buffer)
-        return bytearray(out_buffer[:out_len])
-    
-    def decompress(self, compressed_data, usr_data_length):
-        mv_compr = memoryview(compressed_data)
-        assert(usr_data_length > len(mv_compr))
-        
-        usr_data = memoryview(bytearray(usr_data_length))
-        core.decompress(self.compr_type.value, usr_data, mv_compr)
-        return bytearray(usr_data)
-
-class CompressType(object):
-    def __init__(self, name, value, supported=True):
-        self._name = name
-        self._value = int(value)
-        self._supported = supported
-        self._register()
-    
-    def __str__(self):
-        return self._name
-    __repr__ = __str__
-    
-    @property
-    def value(self):
-        return self._value
-    
-    @property
-    def supported(self):
-        return self._supported
-    
-    NAME_TABLE = {}
-    VALUE_TABLE = {}
-    
-    def _register(self):
-        assert(self._name not in self.NAME_TABLE)
-        assert(self._value not in self.VALUE_TABLE)
-        self.NAME_TABLE[self._name] = self
-        self.VALUE_TABLE[self._value] = self
+    @classmethod
+    def ls(cls):
+        keylen = max([len(alg[2]) for alg in cls.ALGORITHM_TABLE])
+        print('\n'.join([
+            '%-*s : %2d' % (keylen,alg[2],alg[0])
+            for alg in cls.ALGORITHM_TABLE
+        ]))
     
     @classmethod
     def get(cls, key):
-        if isinstance(key,str) and key in cls.NAME_TABLE:
-            return cls.NAME_TABLE[key]
-        try:
-            return cls.VALUE_TABLE[int(key)]
-        except:
-            return None
+        if isinstance(key,str) and key in cls.name_table:
+            return cls.name_table[key]
+        else:
+            try:
+                return cls.value_table[int(key)]
+            except:
+                return None
+    
+    def compress(self, usr_data):
+        self.__check_supported()
+        
+        mv_usr = memoryview(usr_data)
+        out_buffer = memoryview(bytearray(len(mv_usr)-1))
+        out_len = core.compress(self.enum_value, mv_usr, out_buffer)
+        
+        return bytearray(out_buffer[:out_len])
+    
+    def decompress(self, compressed_data, usr_data_length):
+        self.__check_supported()
+        
+        mv_compr = memoryview(compressed_data)
+        assert(usr_data_length > len(mv_compr))
+        usr_data = memoryview(bytearray(usr_data_length))
+        core.decompress(self.enum_value, usr_data, mv_compr)
+        
+        return bytearray(usr_data)
+    
+    name_table,value_table = {},{}
+    
+    def __init__(self, name, enum_name, enum_value, supported):
+        self.name = name
+        self.enum_name = enum_name
+        self.enum_value = enum_value
+        self.supported = supported
+    
+    def __check_supported(self):
+        if not self.supported:
+            raise Exception('%s is not supported' % str(compr_type))
+    
+    def __str__(self):
+        return self.name
+    __repr__ = __str__
+    
+    @classmethod
+    def register(cls):
+        if cls.name_table and cls.value_table:
+            return
+        
+        cls.name_table,cls.value_table = {},{}
+        for alg in cls.ALGORITHM_TABLE:
+            enum_value,enum_name,name,supported=alg
+            inst = cls(
+                name       = name,
+                enum_name  = enum_name,
+                enum_value = enum_value,
+                supported  = supported
+            )
+            cls.name_table[name] = cls.name_table[enum_name] = inst
+            cls.value_table[enum_value] = inst
+            setattr(cls, name, inst)
 
-ZIO_COMPRESS_INHERIT = CompressType('ZIO_COMPRESS_INHERIT', 0, supported=False)
-ZIO_COMPRESS_ON      = CompressType('ZIO_COMPRESS_ON',      1, supported=False)
-ZIO_COMPRESS_OFF     = CompressType('ZIO_COMPRESS_OFF',     2, supported=False)
-ZIO_COMPRESS_LZJB    = CompressType('ZIO_COMPRESS_LZJB',    3)
-ZIO_COMPRESS_EMPTY   = CompressType('ZIO_COMPRESS_EMPTY',   4, supported=False)
-ZIO_COMPRESS_GZIP_1  = CompressType('ZIO_COMPRESS_GZIP_1',  5)
-ZIO_COMPRESS_GZIP_2  = CompressType('ZIO_COMPRESS_GZIP_2',  6)
-ZIO_COMPRESS_GZIP_3  = CompressType('ZIO_COMPRESS_GZIP_3',  7)
-ZIO_COMPRESS_GZIP_4  = CompressType('ZIO_COMPRESS_GZIP_4',  8)
-ZIO_COMPRESS_GZIP_5  = CompressType('ZIO_COMPRESS_GZIP_5',  9)
-ZIO_COMPRESS_GZIP_6  = CompressType('ZIO_COMPRESS_GZIP_6', 10)
-ZIO_COMPRESS_GZIP_7  = CompressType('ZIO_COMPRESS_GZIP_7', 11)
-ZIO_COMPRESS_GZIP_8  = CompressType('ZIO_COMPRESS_GZIP_8', 12)
-ZIO_COMPRESS_GZIP_9  = CompressType('ZIO_COMPRESS_GZIP_9', 13)
-ZIO_COMPRESS_ZLE     = CompressType('ZIO_COMPRESS_ZLE',    14)
-ZIO_COMPRESS_LZ4     = CompressType('ZIO_COMPRESS_LZ4',    15)
-ZIO_COMPRESS_ZSTD    = CompressType('ZIO_COMPRESS_ZSTD',   16, supported=False)
+Compressor.register()
